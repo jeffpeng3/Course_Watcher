0a4b5cdd4ba9f4995f23752fa9c518fc78488c98
jeff <jeffpeng2012@gmail.com>
2025-09-03 21:53:57 +0800

    a

--- main.py
+++ main.py
@@ -27,13 +27,13 @@ async def main():
                     async with session.post(url, json=payload, ssl=False) as response:
                         data = (await response.json())[0]
                         if int(data['Restrict2']) - data['ChooseStudent'] > 0:
-                            print(data['CourseName'], data['CourseNo'])
+                            print(data['CourseName'], data['CourseNo'], flush=True)
                             if webhook:
                                 dc['content'] = f"{data['CourseName']} {data['CourseNo']}"
                                 async with session.post(webhook, json=dc, ssl=False) as r:
-                                    print(await r.text())
+                                    print(await r.text(), flush=True)
                 except Exception as e:
-                    print("pass i because", e)
+                    print("pass i because", e, flush=True)
                 await sleep(0.3)
                 print(end='.', flush=True)
 run(main())
